dbaefb81720846955a9c1e151da58037fd6939d8
Zaneoldridge <Zaneoldridge@gmail.com>
2024-11-01 09:31:51 +1300

    testing server

--- Flask.py
+++ Flask.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for
-
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
+
+    return "hello world"
+
     return render_template('index.html')
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
