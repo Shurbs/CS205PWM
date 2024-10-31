20602fa9dca5c7e099ecd93dcee8e17232311f90
Zaneoldridge <Zaneoldridge@gmail.com>
2024-11-01 11:19:40 +1300

    adding conntection to pages

--- Flask.py
+++ Flask.py
@@ -7,6 +7,13 @@ def index():
 
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+   return render_template('login.html')
+
+@app.route('/signup')
+def login():
+   return render_template('signup.html')
 
 
 
