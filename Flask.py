6cc068f37fd2211e5ddc5abe3e99c5637e607541
Taine Gould <tainegouldus@gmail.com>
2024-11-01 11:31:03 +1300

    fixed flask page to link correctly

--- Flask.py
+++ Flask.py
@@ -1,23 +1,19 @@
 from flask import Flask, render_template, request, redirect, url_for
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route('/')
 def index():
-
     return render_template('index.html')
 
 @app.route('/login')
 def login():
-   return render_template('login.html')
+    return render_template('login.html')
 
 @app.route('/signup')
-def login():
-   return render_template('signup.html')
-
+def signup():
+    return render_template('signup.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-    
