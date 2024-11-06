8bdc7160da6718477ae303da022261cc94c78fed
Zaneoldridge <Zaneoldridge@gmail.com>
2024-11-07 11:15:15 +1300

    working on submit button to link to vault page

--- Flask.py
+++ Flask.py
@@ -6,14 +6,19 @@ app = Flask(__name__, template_folder='templates')
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        return redirect(url_for('vault'))
     return render_template('login.html')
 
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
 
+@app.route('/vault')
+def vault_page():
+   return render_template('vault.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
